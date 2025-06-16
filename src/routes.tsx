4d5baa6f20885ebe45612efc74a5196634b49625
Fernando Andrade <fernandofreamunde@gmail.com>
2025-06-16 17:43:19 +0200

    ✨💄 add page layouts

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,28 @@
 import { createBrowserRouter } from "react-router";
 import { Dashboard } from "./pages/app/dashboard";
 import { Login } from "./pages/auth/login";
+import { AppLayout } from "./pages/_layouts/app";
+import { AuthLayout } from "./pages/_layouts/auth";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Dashboard />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Dashboard />,
+      },
+    ],
   },
   {
-    path: "/login",
-    element: <Login />,
+    path: "/",
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
   },
 ]);
