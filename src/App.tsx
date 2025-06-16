5355091936886af769ff3fa0d7cfa7aa61fc017c
Fernando Andrade <fernandofreamunde@gmail.com>
2025-06-16 16:38:48 +0200

    🔧configure ESLint and Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import { Button } from './components/ui/button'
-import './index.css'
+import { Button } from "./components/ui/button";
+import "./index.css";
 
 export function App() {
-
   return (
-    <div className='flex justify-center items-center h-screen'>
-        <h1 className='bg-amber-400 text-black'>
-      Hello pizza
-    </h1>
-    <Button>click me</Button>
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="bg-amber-400 text-black">Hello pizza</h1>
+      <Button>click me</Button>
     </div>
-
-  )
+  );
 }
