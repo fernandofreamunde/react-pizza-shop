70d593f0c0bbd85cb99c6bde0fdb2b3e312b1ea3
Fernando Andrade <fernandofreamunde@gmail.com>
2025-06-16 16:33:27 +0200

    ⚗️ test chadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import './App.css'
+import { Button } from './components/ui/button'
+import './index.css'
 
 export function App() {
 
   return (
-    <h1>
+    <div className='flex justify-center items-center h-screen'>
+        <h1 className='bg-amber-400 text-black'>
       Hello pizza
     </h1>
+    <Button>click me</Button>
+    </div>
+
   )
 }
