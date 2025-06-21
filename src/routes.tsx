4016a831fd1257bd1a8004d5601562efde993787
Fernando Andrade <fernandofreamunde@gmail.com>
2025-06-21 17:53:05 +0200

    ✨ 404 page not found

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,11 +4,14 @@ import { Login } from "./pages/auth/login";
 import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
 import { Register } from "./pages/auth/registration";
+import { Orders } from "./pages/app/orders/orders";
+import { NotFound } from "./pages/404";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       {
         path: "/",
