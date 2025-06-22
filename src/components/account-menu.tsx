2a0c523c02c19e3857845f9bd81de03428a591f9
Fernando Andrade <fernandofreamunde@gmail.com>
2025-06-22 13:30:56 +0200

    ✨ add loading state to profile

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -11,14 +11,15 @@ import {
 import { useQuery } from "@tanstack/react-query";
 import { getProfile } from "@/api/get-profile";
 import { getManagedRestaurant } from "@/api/get-managed-restaurant";
+import { Skeleton } from "./ui/skeleton";
 
 export function AccountMenu() {
-  const { data: profile } = useQuery({
+  const { data: profile, isLoading: isLoadingProfile } = useQuery({
     queryKey: ["profile"],
     queryFn: getProfile,
   });
 
-  const { data: manageRestaurant } = useQuery({
+  const { data: manageRestaurant, isLoading: isLoadingRestaurant } = useQuery({
     queryKey: ["managed-restaurant"],
     queryFn: getManagedRestaurant,
   });
@@ -30,17 +31,31 @@ export function AccountMenu() {
           variant="outline"
           className="flex items-center gap-2 select-none"
         >
-          {manageRestaurant?.name}
+          {isLoadingRestaurant ? (
+            <Skeleton className="h-4 w-40" />
+          ) : (
+            manageRestaurant?.name
+          )}
+
           <ChevronDown className="h-4 w-4" />
         </Button>
       </DropdownMenuTrigger>
 
       <DropdownMenuContent align="end" className="w-56">
         <DropdownMenuLabel className="flex flex-col">
-          <span>{profile?.name}</span>
-          <span className="text-muted-foreground text-xs font-normal">
-            {profile?.email}
-          </span>
+          {isLoadingProfile ? (
+            <div className="space-y-1.5">
+              <Skeleton className="h-4 w-32" />
+              <Skeleton className="h-3 w-24" />
+            </div>
+          ) : (
+            <>
+              <span>{profile?.name}</span>
+              <span className="text-muted-foreground text-xs font-normal">
+                {profile?.email}
+              </span>
+            </>
+          )}
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuItem>
--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -5,3 +5,25 @@ export const api = axios.create({
   baseURL: env.VITE_API_URL,
   withCredentials: true,
 });
+
+// add delay to simulate slow requests
+if (env.VITE_ENABLE_API_DELAY) {
+  api.interceptors.request.use(async (config) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    return config;
+  });
+}
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response.status === 401) {
+      // Redirecionar para a página de login
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  },
+);
