753116b063979182259b8f21cf58f78a80087674
Fernando Andrade <fernandofreamunde@gmail.com>
2025-06-23 22:57:32 +0200

    ➕🔧 install and configure vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'node:path'
+import { defineConfig, type InlineConfig, type UserConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,4 +11,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  test: {
+    globals: true,
+    setupFiles: ["./test/setup.ts"],
+    environment: "happy-dom",
+  },
+} as UserConfig & {
+  test: InlineConfig;
+});
