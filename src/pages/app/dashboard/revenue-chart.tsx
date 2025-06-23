312c24aa2db76a97d8f87842652c6e8dffc5095c
Fernando Andrade <fernandofreamunde@gmail.com>
2025-06-23 18:23:58 +0200

    ✨ get sales period graph data

--- src/pages/app/dashboard/revenue-chart.tsx
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -17,17 +17,48 @@ import {
   YAxis,
 } from "recharts";
 
-const data = [
-  { date: "23/6", revenue: 1506 },
-  { date: "24/6", revenue: 1085 },
-  { date: "25/6", revenue: 1192 },
-  { date: "26/6", revenue: 1651 },
-  { date: "27/6", revenue: 951 },
-  { date: "28/6", revenue: 1495 },
-  { date: "29/6", revenue: 1213 },
-];
+import { DateRangePicker } from "@/components/ui/date-range-picker";
+import { Label } from "@/components/ui/label";
+import { useMemo, useState } from "react";
+import type { DateRange } from "react-day-picker";
+import { subDays } from "date-fns";
+import { useQuery } from "@tanstack/react-query";
+import { getDailyRevenueInPeriod } from "@/api/get-daily-revenue-in-period";
+
+// const data = [
+//   { date: "23/6", revenue: 1506 },
+//   { date: "24/6", revenue: 1085 },
+//   { date: "25/6", revenue: 1192 },
+//   { date: "26/6", revenue: 1651 },
+//   { date: "27/6", revenue: 951 },
+//   { date: "28/6", revenue: 1495 },
+//   { date: "29/6", revenue: 1213 },
+// ];
 
 export function RevenueChart() {
+  const [dateRange, setDateRange] = useState<DateRange | undefined>({
+    from: subDays(new Date(), 7),
+    to: new Date(),
+  });
+
+  const { data: dailyRevenueInPeriod } = useQuery({
+    queryKey: ["metrics", "daily-revenue-in-period", dateRange],
+    queryFn: () =>
+      getDailyRevenueInPeriod({
+        from: dateRange?.from,
+        to: dateRange?.to,
+      }),
+  });
+
+  const data = useMemo(() => {
+    return dailyRevenueInPeriod?.map((chartItem) => {
+      return {
+        date: chartItem.date,
+        revenue: chartItem.receipt / 100,
+      };
+    });
+  }, [dailyRevenueInPeriod]);
+
   return (
     <Card className="col-span-6">
       <CardHeader className="flex-row items-center justify-between pb-8">
@@ -36,34 +67,41 @@ export function RevenueChart() {
             Period Revenue
           </CardTitle>
           <CardDescription>Period Daily Revenue</CardDescription>
+
+          <div className="flex items-center gap-3">
+            <Label>Period</Label>
+            <DateRangePicker date={dateRange} onDateChange={setDateRange} />
+          </div>
         </div>
       </CardHeader>
 
       <CardContent>
-        <ResponsiveContainer width="100%" height={240}>
-          <LineChart data={data} style={{ fontSize: 12 }}>
-            <XAxis dataKey="date" axisLine={false} tickLine={false} dy={16} />
-            <YAxis
-              stroke="#888"
-              axisLine={false}
-              tickLine={false}
-              width={80}
-              tickFormatter={(value: number) =>
-                value.toLocaleString("en", {
-                  style: "currency",
-                  currency: "EUR",
-                })
-              }
-            />
-            <CartesianGrid vertical={false} className="stroke-muted" />
-            <Line
-              stroke={colors.violet[500]}
-              type="linear"
-              strokeWidth={2}
-              dataKey="revenue"
-            />
-          </LineChart>
-        </ResponsiveContainer>
+        {data && (
+          <ResponsiveContainer width="100%" height={240}>
+            <LineChart data={data} style={{ fontSize: 12 }}>
+              <XAxis dataKey="date" axisLine={false} tickLine={false} dy={16} />
+              <YAxis
+                stroke="#888"
+                axisLine={false}
+                tickLine={false}
+                width={80}
+                tickFormatter={(value: number) =>
+                  value.toLocaleString("en", {
+                    style: "currency",
+                    currency: "EUR",
+                  })
+                }
+              />
+              <CartesianGrid vertical={false} className="stroke-muted" />
+              <Line
+                stroke={colors.violet[500]}
+                type="linear"
+                strokeWidth={2}
+                dataKey="revenue"
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        )}
       </CardContent>
     </Card>
   );
